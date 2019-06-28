d985bcf63c7f851aab10973590082b4197b27c85
Bruno Filipe Ramos Martins <brunofiliperamos@hotmail.com>
2019-06-27 23:29:59 -0300

    Estrutura inicial

---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calendar from 'components/Calendar';
+import { Wrapper, Container } from './styles';
+
+const Main = () => (
+  <Wrapper>
+    <Container>
+      <Calendar />
+    </Container>
+  </Wrapper>
+);
+
+export default Main;
---
+++ src/pages/Main/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  width: 100%;
+  background-color: red;
+  display: flex;
+  justify-content: center;
+`;
+
+export const Container = styled.div`
+  width: 300px;
+`;
