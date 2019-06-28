8e92e21aecbb62535485c1a5fb9d67a29b4aadc4
Bruno Filipe Ramos Martins <brunofiliperamos@hotmail.com>
2019-06-27 23:36:32 -0300

    Modificando estilo

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,12 +1,11 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  width: 100%;
-  background-color: red;
   display: flex;
   justify-content: center;
 `;
 
 export const Container = styled.div`
+  box-shadow: 0 0 18px 0 #dddddd;
   width: 300px;
 `;
